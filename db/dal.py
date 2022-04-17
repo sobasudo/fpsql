a75ed90eef96c26158592c7f3bb7b0a759a7e3f4
Ranmal Dias <dev.mendy.das@gmail.com>
2022-04-18 00:34:49 +0530

    Ready to add DAL

--- db/dal.py
+++ db/dal.py
@@ -1,5 +1,6 @@
-from parser import json_engine, config_engine
-from sqlalchemy import MetaData
+from db.parser import json_engine, config_engine
+from sqlalchemy import Column, MetaData
+import sqlalchemy
 
 
 class Database(object):
@@ -17,6 +18,28 @@ class Database(object):
 class Schema(object):
 
     def __init__(self):
-        
+        self.metadata = MetaData()
+        self.relations = dict()
 
-    
+    def create_table(self, name):
+        if not name:
+            raise RuntimeError("Table must have a name!")
+        if name in self.relations.keys():
+            raise RuntimeError(f"Table names must be unique. {name} exists!")
+        self.relations[name] = LazyTable()
+        return self.relations[name]
+
+    def try_create(self):
+        pass
+
+
+class LazyTable(object):
+
+    def __init__(self):
+        self.columns = list()
+        self.evaluated = False
+
+    def col(self, name, data_type, primary_key=False, nullable=False, unique=False, constraints=None):
+        self.columns.append(Column(name, getattr(
+            sqlalchemy, data_type)(), primary_key=primary_key))
+        return self
--- tests/test_model_api.py
+++ tests/test_model_api.py
@@ -1,18 +1,28 @@
 import unittest
+from db.dal import Database
 
 class TestModelAPI(unittest.TestCase):
-    
+
+    def setUp(self):
+        self.db = Database(config=r".\sample user files\testconfig.ini")
+
     def test_simple_insert(self):
-        db = Database(config=r".\sample user files\testconfig.ini")
-        models = db.create_schema(schema='auth')
-        models.create_model('user')\
+        schema = self.db.create_schema(name='auth')
+        schema.create_table('user')\
             .col('uid', 'Integer', primary_key=True)\
             .col('name', 'String')
-        models.try_create()
-        models['user'].from_dict(data={
+        schema.try_create()
+        schema['user'].from_dict(data={
             'name': 'Ranmal Dias'
         })
         self.assertDictEqual({
             'uid': 1,
             'name': 'Ranmal Dias'
-        }, models['user'].fetch_all())
+        }, schema['user'].fetch_all())
+
+    def test_database_no_config_error(self):
+        self.assertRaises(RuntimeError, Database)
+
+    def test_table_without_name_error(self):
+        self.assertRaises(RuntimeError, self.db.create_schema(
+            name="test").create_table())
